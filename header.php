9147c32cfbac7512f8d87b80bbdfd5afa8ef1463
Nicholas Rowe <nbrowe46@gmail.com>
2015-05-01 08:56:16 +0100

    reorganized front end

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600' rel='stylesheet' type='text/css'>
+
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/global.css ?> ">
 
 	<?php wp_head(); ?>
